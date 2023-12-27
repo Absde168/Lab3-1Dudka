718bc0d795a3c4b012b2c7caac4ff699d7f22584
B3dRandoms <bdrandoms0@gmail.com>
2023-12-27 23:09:32 +0200

    Initial commit

---
+++ Lab3-1Dudka/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите вещественное число X: ");
+double X = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите целое число N (> 0): ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+double result = 0;
+
+for (int i = 0; i <= N; i++)
+{
+    double numerator = Math.Pow(-1, i) * Math.Pow(X, 2 * i);
+    double denominator = 2 * i;
+
+    result += numerator / denominator;
+}
+
+Console.WriteLine("Значение выражения: " + result);
